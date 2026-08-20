425e10afed2895fbc2055bc3c0e3adadfeea8e04
Gnoud42 <vhduong4294@gmail.com>
2026-08-20 13:06:12 +0700

    recreate wrangler.jsonc and fix astro config import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,12 @@
+// https://astro.build/config
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+
+import { remarkReadingTime } from "./src/utils/all";
+
 export default defineConfig({
   site: "https://sccurious.pages.dev",
   markdown: {
