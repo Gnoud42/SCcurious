21b02b1b8be297b65815e1357326e746baed155f
Gnoud42 <vhduong4294@gmail.com>
2026-08-20 13:08:46 +0700

    temporarily disable sitemap to unblock deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+// import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 import { remarkReadingTime } from "./src/utils/all";
@@ -14,5 +14,5 @@ export default defineConfig({
     rehypePlugins: ["rehype-plugin-image-native-lazy-loading"],
     extendDefaultPlugins: true,
   },
-  integrations: [tailwind(), mdx(), sitemap(), icon()],
+  integrations: [tailwind(), mdx(), icon()], //sitemap()
 });
