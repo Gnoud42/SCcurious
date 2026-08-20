bff684e68aba335c63a304a2f111476a4fac776d
Gnoud42 <vhduong4294@gmail.com>
2026-08-20 11:16:14 +0700

    fix site config for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,5 @@
-// https://astro.build/config
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
-
-import { remarkReadingTime } from "./src/utils/all";
-
 export default defineConfig({
-  site: "https://stablo-astro.web3templates.com",
+  site: "https://sccurious.pages.dev",
   markdown: {
     remarkPlugins: [remarkReadingTime],
     rehypePlugins: ["rehype-plugin-image-native-lazy-loading"],
